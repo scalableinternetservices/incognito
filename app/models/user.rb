718915e2fbf71eed989dd3e3029993e5b7f9df3a
Will <willtan510@yahoo.com>
2019-10-22 03:49:56 +0000

    basic signup working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   
-  before_save { self.email = email.downcase, self.username = username.downcase   }
-
+  before_save { self.email = email.downcase   }
+  before_save { self.username = username.downcase }
  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :email, presence: true, length: { maximum: 255 },
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_020116) do
+ActiveRecord::Schema.define(version: 2019_10_22_031052) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
