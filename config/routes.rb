954c160546f09d09e450cf7b4fcee6f674409cf1
raymondlin1 <raymond.lin9126@gmail.com>
2019-10-23 04:32:53 +0000

    working on login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  
+  get 'users/new'
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
   get  '/signup',  to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(username: "ExampleUser2", email: "user@example.com")
+    @user = User.new(username: "ExampleUser2", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
