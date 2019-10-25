68c3077a1ed3eb10c080d31c66dadc6cbe56d6cb
raymondlin1 <raymond.lin9126@gmail.com>
2019-10-25 00:10:33 +0000

    created working login page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  #returns current logged in user
+  def current_user
+    if session[:user_ud]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
