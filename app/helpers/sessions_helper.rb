4f86c5ddf830f300c4835a7f0ab08b8e806eb2f7
Will <willtan510@yahoo.com>
2019-10-31 08:00:17 +0000

    user routing somewhat done, need to fix user being remembered they are logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,4 +9,26 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+  
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
+
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
