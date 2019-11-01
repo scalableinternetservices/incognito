39cc87ad7e61cb24a9e6c352fc7e8719c753cf09
SalYard <ssaa@g.ucla.edu>
2019-10-31 23:25:37 -0700

    Basic Post without commits

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,14 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  #####SALEKH'S ROUTES #########
   get 'posts', to: "posts#index"
   get 'posts/new', to:"posts#new"
+  post 'posts', to:"posts#create"
+  delete 'posts/:id', to:"posts#destroy", as: :delete_post
+  get 'posts/:id', to:"posts#show", as: :post
+  get 'posts/:id/edit', to:"posts#edit", as: :edit_post
+  patch 'posts/:id', to:"posts#update"
+  
+
 end
