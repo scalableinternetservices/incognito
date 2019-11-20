a9a814caba76f9c5d1fac05b1bd417500909054c
raymondlin1 <raymond.lin9126@gmail.com>
2019-11-20 01:19:38 +0000

    added a new route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
+  post 'signup', to: 'users#create'
   
   resources :users
 
