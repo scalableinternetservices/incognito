f144ec56e368db73fc2cc494ba699b2dd0cc197a
Ubuntu <raymond.lin9126@gmail.com>
2019-11-22 07:22:40 +0000

    nothing too important

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,13 +59,13 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
         @post.upvote_by current_user
         redirect_back(fallback_location: root_path)
-      end  
-      
-      def downvote
+    end  
+  
+    def downvote
         @post = Post.find(params[:id])
         @post.downvote_by current_user
         redirect_back(fallback_location: root_path)
-      end
+    end
 
 
     def search  
