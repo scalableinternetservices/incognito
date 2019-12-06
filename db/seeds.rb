0780cf138998f38e1faaa087d72b102d65510b3c
Will <willtan510@yahoo.com>
2019-12-06 05:15:01 +0000

    db seed for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create( email: "blah@ucla.edu", username:"bleh",write:true, university_acronym: "ucla", university: "University of California, Los Angeles", password: "123456", password_confirmation:"123456")
+User.create( email: "blah@usc.edu", username:"bleh2",write:true, university_acronym: "usc", university: "University of Southern California", password: "123456", password_confirmation:"123456")
+User.create( email: "blah@berkeley.edu", username:"bleh3",write:true, university_acronym: "berkeley", university: "University of California, Berkeley", password: "123456", password_confirmation:"123456")
+
+Post.create!(title:  "That Eggert Midterm was EASY",
+             body: "ggwp that was ezpz",
+             user_id: 1,
+             university: "ucla",
+             public: false)
+             
+Post.create!(title:  "Man UCLA Sucks",
+             body: "Go Trojans",
+             user_id: 2,
+             university: "usc",
+             public: true)
+             
+ Post.create!(title:  "It's so sad here in Berkeley :(",
+             body: "^",
+             user_id: 3,
+             university: "berkeley",
+             public: true)
+             
